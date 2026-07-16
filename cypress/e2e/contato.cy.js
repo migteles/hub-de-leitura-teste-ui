fed539d2ea18aa86d4dedb94c14d5aeba804bb67
Miguel Teles <miguelteles371@gmail.com>
2026-07-16 13:47:43 -0400

    Automação da funcionalidade de Contato

---
+++ cypress/e2e/contato.cy.js
@@ -0,0 +1,48 @@
+describe('Funcionalidade: Contato', () => {
+  
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/index.html')
+  });
+  
+  it('Deve preencher o formulário de contato com sucesso', () => {
+    cy.get('[name="name"]').type('Miguel Teles')
+    cy.get('[name="email"]').type('miguelteles@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('#alert-container', 'Contato enviado com sucesso!').should('be.visible')
+  });
+
+  it('Deve validar mensagem de erro sem preencher o nome', () => {
+    cy.get('[name="email"]').type('miguelteles@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('#alert-container', 'Por favor, preencha o campo Nome.').should('be.visible')
+  });
+
+  it('Deve validar mensagem de erro sem preencher o email', () => {
+    cy.get('[name="name"]').type('Miguel Teles')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('#alert-container', 'Por favor, preencha o campo E-mail.').should('be.visible')
+  });
+
+  it('Deve validar mensagem de erro sem selecionar o assunto', () => {
+    cy.get('[name="name"]').type('Miguel Teles')
+    cy.get('[name="email"]').type('miguelteles@teste.com')
+    cy.get('[name="message"]').type('Mensagem de teste')
+    cy.get('#btn-submit').click()
+    cy.contains('#alert-container', 'Por favor, selecione o Assunto.').should('be.visible')
+  });
+
+  it('Deve validar mensagem de erro sem preencher a mensagem', () => {
+    cy.get('[name="name"]').type('Miguel Teles')
+    cy.get('[name="email"]').type('miguelteles@teste.com')
+    cy.get('[name="subject"]').select('Suporte Técnico')
+    cy.get('#btn-submit').click()
+    cy.contains('#alert-container', 'Por favor, escreva sua Mensagem.').should('be.visible')
+  });
+
+});
