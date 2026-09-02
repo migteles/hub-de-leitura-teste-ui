836da016a2dab560727342632c30292239fb975a
Miguel Teles <miguelteles371@gmail.com>
2026-09-02 10:45:25 -0400

    Updated catalogo-busca.cy.js

--- cypress/e2e/catalogo-busca.cy.js
+++ cypress/e2e/catalogo-busca.cy.js
@@ -13,10 +13,28 @@ describe('Funcionalidade: Busca no Catalogo de Livros', () => {
         cy.get('.card-title > .text-dark').should('contain', '1984')
     });
 
-    it.only('Deve buscar um livro do arquivo da massa de dados', () => {
+    it('Deve buscar um livro do arquivo da massa de dados', () => {
         cy.get('#search-input').type(catalogo[0].livro)
         // Resultado
         cy.get('.card-title > .text-dark').should('contain', catalogo[0].livro)
+    });
+
+    it('Deve buscar um livro com fixture', () => {
+        cy.fixture('livros').then((cat) =>{
+            cy.get('#search-input').type(cat[0].livro)
+            // Resultado
+            cy.get('.card-title > .text-dark').should('contain', cat[0].livro)
+        })
+    });
+
+    it.only('Deve buscar um livro com fixture', () => {
+        cy.fixture('livros').then((cat) =>{
+            cat.forEach(item =>{
+                cy.get('#search-input').clear().type(item.livro)
+                // Resultado
+                cy.get('.card-title > .text-dark').should('contain', item.livro)
+            })
+        })
     });
 
 })
