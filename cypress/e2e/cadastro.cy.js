79ff41b660c25d96287fb1b440f5a6b1553a479f
Miguel Teles <miguelteles371@gmail.com>
2026-09-02 14:35:34 -0400

    Added cadastro-page.js and updated cadastro.cy.js

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,17 +1,14 @@
 /// <reference types="cypress"/>
-import { faker } from '@faker-js/faker'
+import { faker } from '@faker-js/faker';
+import cadastroPage from '../support/pages/cadastro-page'
 
 describe('Funcionalidade: Cadastro', () => {
   
   beforeEach(() => {
-    cy.visit('register.html')
-  });
-  
-  afterEach(() => {
-    cy.screenshot()
+    cadastroPage.visitarPaginaCadastro()
   });
 
-  it('Realizar Cadastro com sucesso', () => { 
+  it.skip('Realizar Cadastro com sucesso', () => { 
     let email = faker.internet.email()
     let nome = faker.person.fullName()
     cy.get('#name').type(nome)
@@ -26,7 +23,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#user-name').should('contain', name)
   });
 
-  it('Falhar o Cadastro por não adicionar o nome', () => { 
+  it.skip('Falhar o Cadastro por não adicionar o nome', () => { 
     let email = faker.internet.email()
     cy.get('#email').type(email)
     cy.get('#phone').type('11940028922')
@@ -38,7 +35,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#register-form').should('contain', 'Nome deve ter pelo menos 2 caracteres')
   });
   
-  it('Falhar o Cadastro por não adicionar o email', () => { 
+  it.skip('Falhar o Cadastro por não adicionar o email', () => { 
     let nome = faker.person.fullName()
     cy.get('#name').type(nome)
     cy.get('#phone').type('11940028922')
@@ -50,7 +47,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#register-form').should('contain', 'Email válido é obrigatório')
   });
   
-  it('Falhar o Cadastro por não adicionar a senha', () => { 
+  it.skip('Falhar o Cadastro por não adicionar a senha', () => { 
     let email = faker.internet.email()
     let nome = faker.person.fullName()
     cy.get('#name').type(nome)
@@ -62,7 +59,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#register-form').should('contain', 'Por favor, crie uma senha com pelo menos 6 caracteres.')
   });
   
-  it('Falhar o Cadastro pois as senhas não estão iguais', () => { 
+  it.skip('Falhar o Cadastro pois as senhas não estão iguais', () => { 
     let email = faker.internet.email()
     let nome = faker.person.fullName()
     cy.get('#name').type(nome)
@@ -76,7 +73,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#register-form').should('contain', 'Senhas não coincidem')
   });
 
-  it('Falhar o Cadastro por não confirmar os termos de uso', () => { 
+  it.skip('Falhar o Cadastro por não confirmar os termos de uso', () => { 
     let email = faker.internet.email()
     let nome = faker.person.fullName()
     cy.get('#name').type(nome)
@@ -89,7 +86,7 @@ describe('Funcionalidade: Cadastro', () => {
     cy.get('#register-form').should('contain', 'Você deve aceitar os termos de uso')
   });
 
-  it.only('Realizar cadastro com sucesso usando comandos customizados', () => {
+  it.skip('Realizar cadastro com sucesso usando comandos customizados', () => {
     let email = faker.internet.email()
     let nome = faker.person.fullName()  
     cy.preencherCadastro(
@@ -103,4 +100,19 @@ describe('Funcionalidade: Cadastro', () => {
     cy.url().should('include', 'dashboard.html')
     cy.get('#user-name').should('contain', name)
   });
+
+  it('Realizar cadastro com sucesso usando Page Objects', () => {
+    let email = `teste${Date.now()}@teste.com`
+    cadastroPage.preencherCadastro('nome', email, '40028922', 'teste123', 'teste123')
+    // Resultado
+    cy.url().should('include', 'dashboard.html')
+  });
+
+  it('Cadastro falhar utilizando Page Objects', () => {
+    let email = `teste${Date.now()}@teste.com`
+    cadastroPage.preencherCadastro('', email, '40028922', 'teste123', 'teste123')
+    // Resultado
+    cy.get('#register-form').should('contain', 'Nome deve ter pelo menos 2 caracteres')
+  });
+
 });
