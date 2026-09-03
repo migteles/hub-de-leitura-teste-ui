cacb2fab45bc94ea4ae98bb01c858c9563e7df95
Miguel Teles <miguelteles371@gmail.com>
2026-09-03 09:12:31 -0400

    Updated cadastro.cy.js and cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'hvsv6t',
   allowCypressEnv: false,
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-
     baseUrl: "http://localhost:3000/"
 
   },
