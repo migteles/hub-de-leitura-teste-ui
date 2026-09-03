f689a0e17a30b1365b16648ad93614279faccf71
Miguel Teles <miguelteles371@gmail.com>
2026-09-03 09:18:59 -0400

    Changed default browser to chrome (Electron deprecated)

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   projectId: 'hvsv6t',
   allowCypressEnv: false,
 
+  defaultBrowser: 'chrome',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
