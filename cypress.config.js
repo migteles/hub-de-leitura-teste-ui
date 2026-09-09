b22c728125c0738bd98e9f86cdea6dbfdb93c5fd
Miguel Teles <miguelteles371@gmail.com>
2026-09-09 11:36:08 -0400

    Updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
-const { allureCypress } = require ("allure-cypress/reporter");
 const { defineConfig } = require("cypress");
+const { allureCypress } = require ("allure-cypress/reporter");
 
 module.exports = defineConfig({
   env: {
