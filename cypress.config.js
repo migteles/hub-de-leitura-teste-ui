30edfbdf92d07eef5fc081d37611fbb2e9c38b39
Miguel Teles <miguelteles371@gmail.com>
2026-09-21 10:20:31 -0400

    Updated allure-cypress and added allure-js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ const { allureCypress } = require ("allure-cypress/reporter");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      allureCypress(on, config);
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
       return config;
     },
     baseUrl: "http://localhost:3000/",
